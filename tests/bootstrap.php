41a71111ac42d370670b0ed8736c70d20048fec8
Jansen Price <sumpygump@gmail.com>
2014-03-25 12:54:12 +0000

    Add vendor autoloader guard and fix broken path when not in current dir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,5 +11,10 @@ date_default_timezone_set('America/Chicago');
 
 $root = realpath(dirname(dirname(__FILE__)));
 
-$loader = require_once $root . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+$vendorAutoloadPath = $root . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+if (!file_exists($vendorAutoloadPath)) {
+    die("You must run `composer install` before proceeding\n");
+}
+
+$loader = require_once $vendorAutoloadPath;
 $loader->add('', $root . DIRECTORY_SEPARATOR . 'src');
